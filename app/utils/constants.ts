fb8e8aa77161cf5dc9da11b3ce253a11fe209d7e
benbaessler <mail@benbaessler.com>
2024-02-06 16:58:13 +0000

    refactor: constants

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,11 +1,9 @@
-import { QueryParameter, DuneClient } from "@cowprotocol/ts-dune-client";
 import { NeynarAPIClient } from "@neynar/nodejs-sdk";
 import OpenAI from "openai";
 
-export const HOST = process.env.NEXT_PUBLIC_HOST || "http://localhost:3000";
+export const HOST = process.env.NEXT_PUBLIC_HOST;
 
-export const dune = new DuneClient(process.env.DUNE_API_KEY ?? "");
 export const neynar = new NeynarAPIClient(process.env.NEYNAR_API_KEY ?? "");
 export const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY ?? "" });
 
-export const contractAddress = "0xA188155E15490f293eD800Bc3b3d3394E80E535D"
+export const contractAddress = "0xabEa3454f738c6742985aD759EA4c09D56fE4e9a"
