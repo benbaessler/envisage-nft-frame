39053bda5915234ad5932aede0c67bb4a20eca44
benbaessler <mail@benbaessler.com>
2024-02-06 12:20:38 +0000

    chore: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   // without a title, warpcast won't validate your frame
-  title: "frames.js starter",
-  description: "...",
+  title: "Envisage NFT Generator",
+  description:
+    "Generate your own unique, generative AI art piece in a Farcaster Frame for $DEGEN.",
 };
 
 export default function RootLayout({
