6c5b7fd102ef07c12da1cdad5bc645237022f9d7
benbaessler <mail@benbaessler.com>
2024-02-07 18:22:32 +0000

    chore: finalize frame execution

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,14 @@ import {
   useFramesReducer,
   validateActionSignature,
 } from "frames.js/next/server";
-import { bytesToHexString, getAddressForFid } from "frames.js";
+import {
+  bytesToHexString,
+  getAddressForFid,
+  getFrameMessageFromRequestBody,
+} from "frames.js";
 import Link from "next/link";
 import { DEBUG_HUB_OPTIONS } from "./debug/constants";
+import prisma from "./lib/prisma";
 import {
   HOST,
   neynar,
@@ -57,11 +62,18 @@ export default async function Home({
     previousFrame
   );
 
+  console.log(state);
+
   if (state.page === "minting") {
     try {
-      const fid = validMessage?.data?.frameActionBody?.castId?.fid!;
+      const frameMessage = getFrameMessageFromRequestBody(
+        previousFrame.postBody!
+      );
+
+      const fid = frameMessage.data?.fid!;
+
       const castHash = bytesToHexString(
-        validMessage?.data?.frameActionBody?.castId?.hash!
+        frameMessage.data?.frameActionBody?.castId?.hash!
       );
       const inputText = previousFrame.postBody?.untrustedData.inputText;
 
@@ -74,13 +86,12 @@ export default async function Home({
           >
             <FrameImage src={`${HOST}/missing-prompt.png`} />
             <FrameInput text="A word or phrase to envisage" />
-            <FrameButton onClick={dispatch}>Try again ✨</FrameButton>
+            <FrameButton onClick={dispatch}>Try again</FrameButton>
           </FrameContainer>
         );
       }
 
       // Checks if supply was minted
-      // TODO: create image
       if (await supplyMinted()) {
         return (
           <FrameContainer
@@ -89,7 +100,7 @@ export default async function Home({
             previousFrame={previousFrame}
           >
             <FrameImage src={`${HOST}/supply-minted.png`} />
-            <FrameButton onClick={dispatch}>OOS</FrameButton>
+            <FrameButton onClick={() => {}}>Sold out</FrameButton>
           </FrameContainer>
         );
       }
@@ -106,7 +117,7 @@ export default async function Home({
           >
             <FrameImage src={`${HOST}/no-wallet.png`} />
             <FrameInput text="A word or phrase to envisage" />
-            <FrameButton onClick={dispatch}>Try again ✨</FrameButton>
+            <FrameButton onClick={dispatch}>Try again</FrameButton>
           </FrameContainer>
         );
       }
@@ -122,15 +133,33 @@ export default async function Home({
           >
             <FrameImage src={`${HOST}/already-claimed.png`} />
             <FrameInput text="A word or phrase to envisage" />
-            <FrameButton onClick={dispatch}>Try again ✨</FrameButton>
+            <FrameButton onClick={dispatch}>Try again</FrameButton>
           </FrameContainer>
         );
       }
 
-      // TODO: Check if input was already used
+      // Checks if input was already used
+      const promptUsed = await prisma.prompt.findUnique({
+        where: { id: inputText.toLowerCase() },
+      });
+
+      if (promptUsed) {
+        return (
+          <FrameContainer
+            postUrl="/frames"
+            state={state}
+            previousFrame={previousFrame}
+          >
+            <FrameImage src={`${HOST}/taken.png`} />
+            <FrameInput text="A word or phrase to envisage" />
+            <FrameButton onClick={dispatch}>Try again</FrameButton>
+          </FrameContainer>
+        );
+      }
 
       // Checks if the user tipped at least 999 $DEGEN in the replies
       const cast = await neynar.fetchAllCastsInThread(castHash, fid);
+      console.log(fid);
       const hasTipped = cast.result.casts.some(
         (c) =>
           c.author.fid === fid &&
@@ -146,25 +175,61 @@ export default async function Home({
           >
             <FrameImage src={`${HOST}/no-tip.png`} />
             <FrameInput text="A word or phrase to envisage" />
-            <FrameButton onClick={dispatch}>Try again ✨</FrameButton>
+            <FrameButton onClick={dispatch}>Try again</FrameButton>
+          </FrameContainer>
+        );
+      }
+
+      // Stores input in database
+      try {
+        Promise.all([
+          await prisma.prompt.create({
+            data: {
+              id: inputText.toLowerCase(),
+            },
+          }),
+          await prisma.minting.create({
+            data: {
+              fid,
+            },
+          }),
+        ]);
+      } catch (error) {
+        return (
+          <FrameContainer
+            postUrl="/frames"
+            state={state}
+            previousFrame={previousFrame}
+          >
+            <FrameImage src={`${HOST}/please-wait.png`} />
+            <FrameButton onClick={dispatch}>Try again</FrameButton>
           </FrameContainer>
         );
       }
 
       // Generate AI art
+      console.log("generating");
       const image = await openai.images.generate({
         model: "dall-e-3",
-        prompt: `A modern art piece themed around '${inputText}'`,
+        prompt: `A modern art piece themed around the concept of '${inputText}'`,
       });
 
       // Mints the NFT via thirdweb
       const metadata = {
         name: `"${inputText.charAt(0).toUpperCase() + inputText.slice(1)}"`,
-        description: `A modern art piece themed around the concept of '${inputText}'`,
+        description: `A unique, AI-generated artwork minted via a Farcaster Frame with $DEGEN tips.`,
         image: image.data[0]?.url,
       };
 
+      console.log("minting");
       const nft = await contract.mintTo(address, metadata);
+
+      await prisma.minting.delete({
+        where: {
+          fid,
+        },
+      });
+
       return (
         <FrameContainer
           postUrl="/frames"
@@ -194,13 +259,11 @@ export default async function Home({
         >
           <FrameImage src={`${HOST}/error.png`} />
           <FrameInput text="A word or phrase to envisage" />
-          <FrameButton onClick={dispatch}>Try again ✨</FrameButton>
+          <FrameButton onClick={dispatch}>Try again</FrameButton>
         </FrameContainer>
       );
     }
   }
-
-  console.log(state);
 
   // then, when done, return next frame
   return (
