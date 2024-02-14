58d9a61e4131a28a9762f3d8217f45a6e2b52aba
benbaessler <mail@benbaessler.com>
2024-02-14 12:08:33 +0000

    fix: inngest function

--- app/inngest/functions.ts
+++ app/inngest/functions.ts
@@ -4,20 +4,25 @@ import { openai, contract } from "../utils";
 export const mintNft = inngest.createFunction(
   { id: "mint-nft" },
   { event: "mint-nft" },
-  async ({ event }) => {
+  async ({ event, step }) => {
     const { address, prompt } = event.data;
 
     // Generates the image
-    console.log("generating");
-    const image = await openai.images.generate({
-      model: "dall-e-2",
-      prompt: `A modern art piece themed around the concept of '${prompt}'`,
-    });
+    const image = await step.run(
+      "Generate image",
+      async () =>
+        await openai.images.generate({
+          model: "dall-e-2",
+          prompt: `A modern art piece themed around the concept of '${prompt}'`,
+        })
+    );
 
-    await contract.mintTo(address, {
-      name: `"${prompt}"`,
-      description: `A unique, AI-generated, "${prompt}"-themed artwork minted via a Farcaster Frame with $DEGEN tips.`,
-      image: image.data[0]?.url,
+    await step.run("Mint NFT", async () => {
+      await contract.mintTo(address, {
+        name: `"${prompt}"`,
+        description: `A unique, AI-generated, "${prompt}"-themed artwork minted via a Farcaster Frame with $DEGEN tips.`,
+        image: image.data[0]?.url,
+      });
     });
 
     return true;
