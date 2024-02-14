af10f346c7cfa93184a281bd3706b656dbb64838
benbaessler <mail@benbaessler.com>
2024-02-14 14:06:39 +0000

    chore: remove unused function

--- app/utils/contract.ts
+++ app/utils/contract.ts
@@ -1,7 +1,6 @@
 import { BaseSepoliaTestnet, Base } from "@thirdweb-dev/chains";
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 import { contractAddress, openai } from ".";
-import { BigNumberish } from "ethers";
 
 export const thirdweb = ThirdwebSDK.fromPrivateKey(
   process.env.PRIVATE_KEY!,
@@ -25,26 +24,4 @@ export const alreadyClaimed = async (address: string): Promise<boolean> => {
 export const supplyMinted = async () => {
   const totalSupply = await contract.totalSupply();
   return totalSupply.toNumber() > 999;
-};
-
-export const generateAndSetImage = async (
-  tokenId: BigNumberish,
-  prompt: string
-) => {
-  // Generate AI art
-  console.log("generating");
-  const image = await openai.images.generate({
-    model: "dall-e-3",
-    prompt: `A modern art piece themed around the concept of '${prompt}'`,
-  });
-
-  // Mints the NFT via thirdweb
-  const metadata = {
-    name: `"${prompt}"`,
-    description: `A unique, AI-generated, "${prompt}"-themed artwork minted via a Farcaster Frame with $DEGEN tips.`,
-    image: image.data[0]?.url,
-  };
-
-  // Update metadata
-  await contract.erc721.update(tokenId, metadata);
 };
