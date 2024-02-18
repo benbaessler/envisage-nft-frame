1346266f085a32f976fde919838baaafb2d38aa5
benbaessler <mail@benbaessler.com>
2024-02-18 21:48:32 +0000

    feat: handle inngest issues

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../inngest/client";
-import { mintNft } from "../../inngest/functions";
+import { mintNft, handleFailed } from "../../inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [mintNft],
+  functions: [mintNft, handleFailed]
 });
--- app/inngest/functions.ts
+++ app/inngest/functions.ts
@@ -1,5 +1,40 @@
 import { inngest } from "./client";
 import { openai, mintWithPaymaster } from "../utils";
+import * as Sentry from "@sentry/node";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+});
+
+export const handleFailed = inngest.createFunction(
+  {
+    name: "Send failures to Sentry",
+    id: "send-failed-function-errors-to-sentry",
+  },
+  { event: "inngest/function.failed" },
+  async ({ event, step }) => {
+    // The error is serialized as JSON, so we must re-construct it for Sentry's error handling:
+    const error = event.data.error;
+    const reconstructedEvent = new Error(error.message);
+    // Set the name in the newly created event:
+    // You can even customize the name here if you'd like,
+    // e.g. `Function Failure: ${event.} - ${error.name}`
+    reconstructedEvent.name = error.name;
+
+    // Add the stack trace to the error:
+    reconstructedEvent.stack = error.stack;
+
+    // Capture the error with Sentry and append any additional tags or metadata:
+    Sentry.captureException(reconstructedEvent, {
+      extra: {
+        run_id: event.data.run_id,
+      },
+    });
+
+    // Flush the Sentry queue to ensure the error is sent:
+    return await Sentry.flush();
+  }
+);
 
 export const mintNft = inngest.createFunction(
   { id: "mint-nft" },
